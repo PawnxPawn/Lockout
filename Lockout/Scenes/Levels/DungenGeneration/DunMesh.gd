cc63edd49f43ff221a70853e939d8f0363ae1291
Powder Valentine <wildfire0502@gmail.com>
2026-06-26 21:18:49 -0400

    fixed the generation but not sure how to make it visable when playing the scene

--- Lockout/Scenes/Levels/DungenGeneration/DunMesh.gd
+++ Lockout/Scenes/Levels/DungenGeneration/DunMesh.gd
@@ -1,43 +1,42 @@
 @tool
 extends Node3D
-
 @export var grid_map_path : NodePath
 @onready var grid_map : GridMap = get_node(grid_map_path)
-
 @export var start : bool = false : set = set_start
-var dun_cell_scene : PackedScene = preload("res://Scenes/Levels/DungenGeneration/DunCell.blend")
-func set_start(val:bool)->void:
+var dun_cell_scene : PackedScene = preload("res://Scenes/Levels/DungenGeneration/DunCell.tscn")
+func set_start(_val:bool)->void:
 	if Engine.is_editor_hint():
 		create_dungeon()
 var directions : Dictionary = {
 	"up" : Vector3i.FORWARD, "down" : Vector3i.BACK,
 	"left" : Vector3i.LEFT, "right" : Vector3i.RIGHT
 }
-
-func handle_none(cell:Node3D,dir:String):
+func handle_none(cell:Node3D, dir:String):
 	cell.call("remove_door_"+dir)
-func handle_00(cell:Node3D,dir:String):
+func handle_00(cell:Node3D, dir:String):
 	cell.call("remove_wall_"+dir)
 	cell.call("remove_door_"+dir)
-func handle_01(cell:Node3D,dir:String):
-	cell.call("remove_door_"+dir)
-func handle_02(cell:Node3D,dir:String):
+func handle_01(cell:Node3D, dir:String):
 	cell.call("remove_wall_"+dir)
 	cell.call("remove_door_"+dir)
-func handle_10(cell:Node3D,dir:String):
-	cell.call("remove_door_"+dir)
-func handle_11(cell:Node3D,dir:String):
+func handle_02(cell:Node3D, dir:String):
 	cell.call("remove_wall_"+dir)
 	cell.call("remove_door_"+dir)
-func handle_12(cell:Node3D,dir:String):
+func handle_10(cell:Node3D, dir:String):
 	cell.call("remove_wall_"+dir)
 	cell.call("remove_door_"+dir)
-func handle_20(cell:Node3D,dir:String):
+func handle_11(cell:Node3D, dir:String):
 	cell.call("remove_wall_"+dir)
 	cell.call("remove_door_"+dir)
-func handle_21(cell:Node3D,dir:String):
+func handle_12(cell:Node3D, dir:String):
 	cell.call("remove_wall_"+dir)
-func handle_22(cell:Node3D,dir:String):
+	cell.call("remove_door_"+dir)
+func handle_20(cell:Node3D, dir:String):
+	cell.call("remove_wall_"+dir)
+	cell.call("remove_door_"+dir)
+func handle_21(cell:Node3D, dir:String):
+	cell.call("remove_wall_"+dir)
+func handle_22(cell:Node3D, dir:String):
 	cell.call("remove_wall_"+dir)
 	cell.call("remove_door_"+dir)
 func create_dungeon():
@@ -47,19 +46,27 @@ func create_dungeon():
 	var t : int = 0
 	for cell in grid_map.get_used_cells():
 		var cell_index : int = grid_map.get_cell_item(cell)
-		if cell_index <=2\
-		&& cell_index >=0:
-			var dun_cell : Node3D = dun_cell_scene.instantiate() 
-			dun_cell.position = Vector3(cell) + Vector3(0.5,0,0.5)
+		if cell_index >= 0 && cell_index <= 2:
+			var dun_cell : Node3D = dun_cell_scene.instantiate()
+			dun_cell.position = grid_map.map_to_local(cell)
 			add_child(dun_cell)
-			t +=1
-			for i in 4:
-				var cell_n : Vector3i = cell + directions.values()[i]
+			t += 1
+			
+			var orientation : int = grid_map.get_cell_item_orientation(cell)
+			var basis : Basis = grid_map.get_basis_with_orthogonal_index(orientation)
+			
+			for dir_name in directions.keys():
+				var raw_dir : Vector3i = directions[dir_name]
+				var rotated_dir : Vector3i = Vector3i(basis * Vector3(raw_dir))
+				var cell_n : Vector3i = cell + rotated_dir
 				var cell_n_index : int = grid_map.get_cell_item(cell_n)
-				if cell_n_index ==-1\
-				|| cell_n_index ==3:
-					handle_none(dun_cell,directions.keys()[i])
+				
+				if cell_n_index == -1 || cell_n_index == 3:
+					handle_none(dun_cell, dir_name)
 				else:
-					var key : String = str(cell_index) +  str(cell_n_index)
-					call("handle_"+key,dun_cell,directions.keys()[i])
-		if t%10 == 9 : await get_tree().create_timer(0).timeout
+					var key : String = str(cell_index) + str(cell_n_index)
+					if has_method("handle_" + key):
+						call("handle_" + key, dun_cell, dir_name)
+		
+		if t % 10 == 9:
+			await get_tree().create_timer(0).timeout
